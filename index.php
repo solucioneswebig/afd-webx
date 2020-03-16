37014bd8d103c37c5bc5a82c667d873bafb4d93b
solucioneswebig <alexanderr677@gmail.com>
2020-03-16 01:05:39 -0400

    Pruebas nuevo registro

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
 
-//controllers
-require_once "controladores/plantilla.controlador.php";
-require_once "controladores/usuarios.controlador.php";
+
 
 //modelos
 require_once "modelos/ruta.php";
 require_once "modelos/usuarios.model.php";
 require_once "modelos/funciones-bd.php";
+
+//controllers
+require_once "controladores/plantilla.controlador.php";
+require_once "controladores/usuarios.controlador.php";
 
 //plugin
 require_once "librerias/mailer/class.phpmailer.php";
